6f73acf17843094dc26267230e74a24bcb2c4fcb
BETA™ <ardabayardev@gmail.com>
2025-12-21 17:03:43 +0300

    GrandFinale

---
+++ AssignmentCore/Helper/MarkdownHelper.cs
@@ -0,0 +1,25 @@
+﻿using Ganss.Xss;
+using Markdig;
+
+namespace AssignmentCore.Helpers
+{
+    public static class MarkdownHelper
+    {
+        private static readonly MarkdownPipeline Pipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .UseAutoLinks()
+            .UseSoftlineBreakAsHardlineBreak()
+            .Build();
+
+        private static readonly HtmlSanitizer Sanitizer = new HtmlSanitizer();
+
+        public static string ToSafeHtml(string? markdown)
+        {
+            if (string.IsNullOrWhiteSpace(markdown)) return "";
+
+            var html = Markdown.ToHtml(markdown, Pipeline);
+
+            return Sanitizer.Sanitize(html);
+        }
+    }
+}
